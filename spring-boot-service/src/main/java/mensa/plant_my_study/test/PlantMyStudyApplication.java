cb5326b30d730dc7604655cad16f5d7b1aab9f24
jankotanime <jan.gasztold@gmail.com>
2025-07-13 15:29:59 +0200

    add: spring boot service

---
+++ spring-boot-service/src/main/java/mensa/plant_my_study/test/PlantMyStudyApplication.java
@@ -0,0 +1,11 @@
+package mensa.plant_my_study.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantMyStudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlantMyStudyApplication.class, args);
+    }
+}
---
+++ spring-boot-service/src/test/java/mensa/plant_my_study/PlantMyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package mensa.plant_my_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantMyStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
