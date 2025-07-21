6b71aeb1ee56261488afcf182b258822d6efb7d8
jankotanime <jan.gasztold@gmail.com>
2025-07-21 23:43:40 +0200

    add: logout backend See #31

--- spring-boot-service/src/main/java/mensa/plant_my_study/refreshToken/RefreshTokenRepository.java
+++ spring-boot-service/src/main/java/mensa/plant_my_study/refreshToken/RefreshTokenRepository.java
@@ -1,12 +1,16 @@
 package mensa.plant_my_study.refreshToken;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import mensa.plant_my_study.user.User;
+
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
-  Optional<RefreshToken> findByToken(String token);
+  Optional<RefreshToken> findById(UUID tokenId);
+  List<RefreshToken> findAllByUser(User user);
 }
